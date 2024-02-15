02bc16d80ed30b510457f46f1058561c3f71d43c
Janessa <lvlajzhao1983@gmail.com>
2024-02-15 09:59:31 +0800

    update schema for user

--- db/user.js
+++ db/user.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+const initCounterSchema = require('./counter')
 
 const initUserSchema = (mongoose) => {
     const userSchema = new Schema({
+        id: { type: Number,  unique: true, index: true, min: 1 },
         name: {
             type: String,
             required: [true, 'name is required'],
@@ -12,7 +14,24 @@ const initUserSchema = (mongoose) => {
             required: [true, 'address is required'],
         },
         intro: String
-    })
+    }, { timestamps: true })
+
+    const CounterModel = initCounterSchema(mongoose)
+
+    userSchema.pre('save', async function (next) {
+        if (!this.isNew) {
+          next();
+          return;
+        }
+        
+        try {
+          const seq = await CounterModel.increment('userId')
+          this.id = seq
+          next()
+        } catch(err) {
+          next(err)
+        }   
+    });
     return mongoose.model('User', userSchema)
 }
 
