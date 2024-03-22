936f347a0568dca06724bd0f589c77e19636ede8
Janessa <lvlajzhao1983@gmail.com>
2024-03-22 10:15:53 +0800

    add event monitor to contract instance

--- config/configParsers/contract.js
+++ config/configParsers/contract.js
@@ -17,8 +17,23 @@ module.exports = class Contract {
         this.#provider = provider
         this.#tokenStandard = tokenStandard
         this.#instance = new ethers.Contract(address, abi, provider)
+        this.#instance.on('mint_tracer', (to, uri) => {
+            logger.debug('Received from mint_tracer event: ', 'to =', to, ' uri =', uri)
+        })
+        this.#instance.on('mintBatch_tracer', (to, uris) => {
+            logger.debug('Received from mintBatch_tracer event: ', 'to =', to, ' uri =', uris)
+        })
         this.#instance.on('buy_tracer', (from, to, ids) => {
             logger.debug('Received from buy_tracer event: ', 'from =', from, 'to =', to, ' ids =', ids)
+        })
+        this.#instance.on('doSafeBuy_tracer', (from, to, ids) => {
+            logger.debug('Received from doSafeBuy_tracer event: ', 'from =', from, 'to =', to, ' ids =', ids)
+        })
+        this.#instance.on('buyBatch_tracer', (froms, to, idss) => {
+            logger.debug('Received from buyBatch_tracer event: ', 'from =', froms, 'to =', to, ' idss =', idss)
+        })
+        this.#instance.on('doSafeBuyBatch_tracer', (froms, to, idss) => {
+            logger.debug('Received from doSafeBuyBatch_tracer event: ', 'from =', froms, 'to =', to, ' idss =', idss)
         })
     }
 
